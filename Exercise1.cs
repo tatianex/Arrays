abc22815a9f873118e23ba86b0cdb503628f8a37
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-07 16:35:46 -0300

    fixed exercise 1

--- Exercise1.cs
+++ Exercise1.cs
@@ -7,14 +7,35 @@ namespace arrays {
             Console.Write("Exercício 1.\nLeia dois arrays A e B com 15 elementos.\n");
             Console.WriteLine("Construir um array C, onde cada elemento de C é a subtração do elemento correspondente de A com B.\n");
 
-            int[] a = new int[15] {44, 28, 59, 68, 63, 55, 42, 89, 75, 84, 104, 79, 78, 81, 96};
-            int[] b = new int[15] {17, 19, 5, 10, 45, 50, 23, 35, 34, 14, 12, 25, 28, 39, 40};
-            int[] c = new int[15];
+            const int arraysLenght = 3;
 
-            for (int i = 0; i < 15; i++) {
+            var a = new double[arraysLenght];
+            var b = new double[arraysLenght];
+            var c = new double[arraysLenght];
+
+            for (int i = 0; i < arraysLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1}º número da primeira lista");
+                a[i] = Double.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < arraysLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1}º número da segunda lista");
+                b[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nA primeira lista contém: ");
+            foreach (double value in a) Console.Write($"{value}, ");
+            Console.WriteLine("\nA segunda lista contém: ");
+            foreach (double value in b) Console.Write($"{value}, ");
+
+            Console.WriteLine("\nA terceira lista contém: ");
+            for (int i = 0; i < arraysLenght; i++) {
                 c[i] = a[i] - b[i];
                 Console.Write($"{c[i]}, ");
             }
+
             Console.ReadLine();
         }
     }
